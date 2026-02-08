3bbf8709a0492678c676d462d5a66f5e5ae0e61e
Amith Sai <amith@Amiths-MacBook-Air.local>
2026-02-08 13:04:45 +0530

    fix: removed retry policy as its only for cloud run

--- terraform/modules/triggers/main.tf
+++ terraform/modules/triggers/main.tf
@@ -14,9 +14,5 @@ resource "google_eventarc_trigger" "storage_trigger" {
     workflow = "projects/${var.project_id}/locations/${var.project_region}/workflows/${var.workflow_name}"
   }
 
-  retry_policy {
-    max_attempts = 1
-  }
-
   service_account = var.service_account_name
 }
